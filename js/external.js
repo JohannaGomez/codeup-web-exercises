d539d68b569b2792dd8903c904575a49b82d6d52
Johanna Gomez <pao_gomez1@hotmail.com>
2017-10-11 16:13:38 -0500

    javascript practices - inline and external

---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+var favoriteColor = prompt("what is your favorite color?");
+alert("great!!!! " + favoriteColor + " is my favorite color too!!");
