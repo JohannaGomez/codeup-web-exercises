7fa3d5d491c85f970af1bfb92d0bbb234067d58f
Johanna Gomez <pao_gomez1@hotmail.com>
2017-10-17 10:54:03 -0500

    ice cream exercise completed with Vivian Help

--- js/while.js
+++ js/while.js
@@ -77,6 +77,7 @@
 //     console.log(i);
 // }
 // **********************************2nd Exercise loops ****************************
+//     Create a while loop that uses console.log() to create the output shown below.
 // var x = 2;
 // var y = 1;
 // while (y <=16) {
@@ -86,26 +87,30 @@
 //
 // }
 // **********************************3rd Exercise loops ****************************
+//     An ice cream seller can't go home until she sells all of her cones.
+// Write a JS program that generates a random number between 50 and 100
+// representing the amount of cones to sell. Your code should generate numbers between 1 and 5,
+// simulating the amount of cones being bought by her clients. Use a do-while loop
+// to log to the console the amount of cones sold to each person.
+// This is how you get the random numbers.
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
-
+// // This expression will generate a random number between 1 and 5
+console.log("This is the number of cones you have to sell: " + allCones);
 do {
+    var conesBought = Math.floor(Math.random() * 5) + 1;
+    if (conesBought > allCones) {
+        console.log("sorry, I don't have enough cones");
+        continue;
+    }
+    allCones = allCones - conesBought;
+    console.log("this client bought " + conesBought + ".  You have " + allCones + " left to sell");
+} while (allCones > 0);
+console.log("yay!! you sold all the cones");
 
-    console.log("you still have " + allCones + "to sell!!!");
-} while (allCones <=100);
-// function isNumeric(number) {
-//     return !isNaN(number);
-// }
-//
-// console.log("start!")
-// var usersNumber;
-// do {
-//     usersNumber = prompt("enter a number");
-// }  while (!isNumeric(usersNumber) || usersNumber === "")
-//
-// console.log(parseInt(usersNumber) + 10);
-// console.log("done!!");
+
+    // 5 cones sold...  // if there are enough cones
+    //     Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+    //     Yay! I sold them all! // If there are no more cones
 
 })();
