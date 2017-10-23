7967ac5d2efa9573748ef31b4b923c50576b7f79
Johanna Gomez <pao_gomez1@hotmail.com>
2017-10-23 10:48:45 -0500

    edited a little bit the circle exercise

--- js/circle.js
+++ js/circle.js
@@ -3,22 +3,22 @@
 
     // create a circle object
     var circle = {
-        radius: 5,
+        radius: 3,
 
-        getArea: function(rad) {
+        getArea: function() {
             // TODO: complete this method
-            var area = Math.PI * (rad * rad);
+            var area = Math.PI * (this.radius * this.radius);
             return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
             if (doRounding) {
-                console.log("Rounded area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea(this.radius)));
-                return Math.round(this.getArea(this.radius));
+                console.log("Rounded area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
+                return Math.round(this.getArea());
             } else {
-                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea(this.radius));
-                return this.getArea(this.radius);
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+                return this.getArea();
             }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
@@ -35,61 +35,12 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
-
-//     *********************BONUS 1 (create a dog object):
-    //The dog object should have properties for:
-    // breed (string),
-    //     weightInPounds (number),
-    //     age (number),
-    //     color (string),
-    //     sterilized (boolean),
-    //     shotRecords (array of objects with properties for date and typeOfShot)//
-    var dog = {
-        breed: "",
-        weightInPounds: 0,
-        age: 0,
-        color: "",
-        sterilized: true,
-        shotRecords: {
-            date: 00/00/00,
-            typeOfShot: ""
-        },
-        bark: function (woof) {
-            console.log("woof from " + this.dog);
-        },
-        getOlder: function (age) {
-
-        }
-
-    }
-//     The dog object should have methods to:
-//         bark() - will console.log “Woof!”
-//         getOlder() - will increase age by 1
-//     fix() - will set sterile to true if dog sterilized property is false
-//     vaccinate() - takes in an argument for the name of the shot and adds a new shot with the current date to the shotRecords array
-//     BONUS 2 (expanding on the books object exercise):
-//     Add a property “keywords” that contains an array of possible genres the book may be categorized by
-//     Add a boolean property “available” and set it to true
-//     Add a dateAvailable property that has a string of the date/time when the book will be available
-//     Add a method lend() that...
-//     - changes the available property to false if it is not already false
-//     - sets the dateAvailable to a date exactly two weeks from when the lend() method is called
-//     (to do this, research the JS Date object and use methods from it in your code)
-//     Add a method receive() that...
-//     - changes the available property to true
-//     - changes the dateAvailable property to the string “now”
-// BONUS 3 (expanding on the books object exercise):
-//     Create an application to take in user input to build the books array of objects.
-//         Allow the user to continue adding books or to finish adding books.
-//         Once the books have been added, output the books array in the console.
-//         Allow a user to delete a book or a group of books by title or author last name
-//     Allow a user to edit a book by index number in the books array
 })();
 
