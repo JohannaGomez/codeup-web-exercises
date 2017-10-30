391d18db28917760b2d404b5af51dc1d25dbc678
Johanna Gomez <pao_gomez1@hotmail.com>
2017-10-30 16:24:10 -0500

    jquery

--- js/object_exercise.js
+++ js/object_exercise.js
@@ -45,16 +45,93 @@ deactivateMembers(members);
 //     The objects should be named, canOfSoda1, canOfSoda2, canOfSoda3.
 // 4. Iterate over the cansOfSoda array and open() and pourOut() each canOfSoda object
 // 5. Console log the cansOfSoda array to check if the methods worked correctly.
+
+
+//****************************** Using dot syntax ********************
 var canOfSoda = {};
 canOfSoda.isEmpty = false;
 canOfSoda.isOpen = false;
 canOfSoda.open = function () {
-    if (canOfSoda.isOpen = false) {
+    if (canOfSoda.isOpen == false) {
         return canOfSoda.isOpen = true;
     };
 };
 canOfSoda.pourOut = function () {
-    if (canOfSoda.isEmpty = false) {
+    if (canOfSoda.isEmpty == false) {
         return canOfSoda.isEmpty = true;
     };
 };
+
+// *********************************** using object literal syntax ************************
+var canOfSoda = {
+  isEmpty: false,
+  isOpen: false,
+  open: function () {
+      if (canOfSoda.isOpen == false) {
+      return canOfSoda.isOpen = true;
+      }
+  },
+  pourOut: function () {
+      if (canOfSoda.isEmpty == false) {
+      return canOfSoda.isEmpty = true;
+      }
+  }
+};
+
+// *******************************
+// 3. Create an array call cansOfSoda containing of 3 canOfSoda objects with the same properties and values as described above.
+//     The objects should be named, canOfSoda1, canOfSoda2, canOfSoda3.
+
+var cansOfSosda = [
+    {
+        name: "canOfSoda1",
+        isEmpty: false,
+        isOpen: false,
+        open: function () {
+            if (canOfSoda.isOpen == false) {
+                return canOfSoda.isOpen = true;
+            }
+        },
+        pourOut: function () {
+            if (canOfSoda.isEmpty == false) {
+                return canOfSoda.isEmpty = true;
+            }
+        }
+    },
+    {
+        name: "canOfSoda2",
+        isEmpty: false,
+        isOpen: false,
+        open: function () {
+            if (canOfSoda.isOpen == false) {
+                return canOfSoda.isOpen = true;
+            }
+        },
+        pourOut: function () {
+            if (canOfSoda.isEmpty == false) {
+                return canOfSoda.isEmpty = true;
+            }
+        }
+    },
+    {
+        name: "canOfSoda3",
+        isEmpty: false,
+        isOpen: false,
+        open: function () {
+            if (canOfSoda.isOpen == false) {
+                return canOfSoda.isOpen = true;
+            }
+        },
+        pourOut: function () {
+            if (canOfSoda.isEmpty == false) {
+                return canOfSoda.isEmpty = true;
+            }
+        }
+    }
+];
+//
+// 4. Iterate over the cansOfSoda array and open() and pourOut() each canOfSoda object
+cansOfSosda.forEach(function (element, index, array) {
+    element.open(true);
+    element.pourOut(true);
+});
