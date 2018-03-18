7a4d72be4c0e0436c5028e42999213b5cc3c2cc9
Johanna Gomez <pao_gomez1@hotmail.com>
2018-03-18 14:37:06 -0500

    functions review for interview

--- js/external.js
+++ js/external.js
@@ -1,6 +1,10 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
-alert('Welcome to my Website!');
-var favoriteColor = prompt("what is your favorite color?");
-alert("great!!!! " + favoriteColor + " is my favorite color too!!");
+// alert('Welcome to my Website!');
+// var favoriteColor = prompt("what is your favorite color?");
+// alert("great!!!! " + favoriteColor + " is my favorite color too!!");
+
+alert('this is another test');
+var colorFavorito = prompt('cual es tu color favorito?');
+alert("coincidencia!! mi color favorito es " + colorFavorito + " tambien!!");
